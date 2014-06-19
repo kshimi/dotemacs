11ca6f24da9e8e42daaa7863d48d08bbf1d30b08
kshimi <katsuyashimizu@gmail.com>
2014-06-19 22:21:33 +0900

    fix mew setting

--- init.d/init-mew.el
+++ init.d/init-mew.el
@@ -3,19 +3,22 @@
 ;;; Commentary:
 ;;; Code:
 
-(require 'mew)
-(setq mew-mail-path "~/Dropbox/Mail")
-(autoload 'mew-user-agent-compose "mew" nil t)
-(if (boundp 'mail-user-agent)
-    (setq mail-user-agent 'mew-user-agent))
-(if (fboundp 'define-mail-user-agent)
-    (define-mail-user-agent
-      'mew-user-agent
-      'mew-user-agent-compose
-      'mew-draft-send-message
-      'mew-draft-kill
-      'mew-send-hook))
-
+(add-hook 'after-init-hook
+	  (lambda ()
+	    
+	    (require 'mew)
+	    (setq mew-mail-path "~/Dropbox/Mail")
+	    (autoload 'mew-user-agent-compose "mew" nil t)
+	    (if (boundp 'mail-user-agent)
+		(setq mail-user-agent 'mew-user-agent))
+	    (if (fboundp 'define-mail-user-agent)
+		(define-mail-user-agent
+		  'mew-user-agent
+		  'mew-user-agent-compose
+		  'mew-draft-send-message
+		  'mew-draft-kill
+		  'mew-send-hook))
+	    ))
 
 (provide 'init-mew)
 ;;; init-mew.el ends here
