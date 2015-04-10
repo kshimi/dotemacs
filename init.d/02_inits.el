03245e67d39f7961d30f62610c126d3a766307dd
SHIMIZU Katsuya <katsuyashimizu@gmail.com>
2015-04-10 21:43:28 +0900

    set Backspace key for Help

--- init.d/02_inits.el
+++ init.d/02_inits.el
@@ -3,5 +3,6 @@
 
 (load-library "term/bobcat")
 (when (fboundp 'terminal-init-bobcat) (terminal-init-bobcat))
+(define-key input-decode-map (kbd "<backspace>") (kbd "C-h"))
 
 (setq eww-search-prefix "http://www.google.com/search?q=")
