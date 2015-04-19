274059a0a5d7a75820d82363ccaec3c23888e5b0
kshimi <katsuyashimizu@gmail.com>
2015-04-19 15:17:52 +0900

    modify windows size for mac

--- init.d/cocoa-emacs-frame.el
+++ init.d/cocoa-emacs-frame.el
@@ -1,7 +1,7 @@
 ;; set frame size
 (setq initial-frame-alist
-      '((top . 22) (left . 1040)
-	(height . 99) (width . 211)))
+      '((top . 22) (left . 840)
+	(height . 99) (width . 240)))
 
 ;; set input method for mac
 (setq default-input-method "MacOSX"))
