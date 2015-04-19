5c8fe0a2d8d314c354e3c8e9bcac16b221abdc5e
kshimi <katsuyashimizu@gmail.com>
2015-04-19 15:00:19 +0900

    amend migemo setting for mac

--- init.d/10_migemo.el
+++ init.d/10_migemo.el
@@ -1,6 +1,6 @@
 ;; migemo
 (when (package-installed-p 'migemo)
-  (when (memq window-system '(darwin))
+  (when (memq window-system '(ns))
     (setq migemo-command "/usr/local/bin/cmigemo")
     (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
     )
