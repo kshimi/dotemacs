32b48bfeb4c99ed6e512d60894ddb69c6e829e95
t0962801 <shimizu.kct@om.asahi-kasei.co.jp>
2016-01-27 18:31:24 +0900

    improve windows settings

--- init.d/02_inits.el
+++ init.d/02_inits.el
@@ -5,7 +5,8 @@
 (when (fboundp 'terminal-init-bobcat) (terminal-init-bobcat))
 (define-key input-decode-map (kbd "<backspace>") (kbd "C-h"))
 
-(when (package-installed-p 'exec-path-from-shell)
-  (exec-path-from-shell-initialize))
+(unless (eq (window-system) 'w32)
+  (when (package-installed-p 'exec-path-from-shell)
+    (exec-path-from-shell-initialize)))
 
 (setq eww-search-prefix "http://www.google.com/search?q=")
--- init.d/windows-frame.el
+++ init.d/windows-frame.el
@@ -1,10 +1,17 @@
-(if (window-system)
+(when (eq (window-system) 'w32)
+
+  (if (equal system-name "PCS40513")
+      (setq initial-frame-alist
+	    '((top . 0) (left . 0)
+	      (height . 57) (width . 88)))
     (setq initial-frame-alist
 	  '((top . 0) (left . 700)
 	    (height . 77) (width . 88))))
-(w32-ime-initialize)
-(setq default-input-method "W32-IME")
-(setq w32-ime-show-mode-line t)
+
+  (w32-ime-initialize)
+  (setq default-input-method "W32-IME")
+  (setq w32-ime-show-mode-line t)
+  )
 
 (custom-set-faces
  '(default ((t (:family #("ＭＳ ゴシック" 0 7 (charset cp932-2-byte)) :foundry "outline" :slant normal :weight normal :height 90 :width normal))))
@@ -12,3 +19,4 @@
 
 (prefer-coding-system 'utf-8)
 (setq default-process-coding-system 'utf-8)
+(setq default-file-name-coding-system 'japanese-cp932-dos)
