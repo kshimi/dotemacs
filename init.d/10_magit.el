d82490ff7747a63e914dbf36f7a4ac4886e40117
t0962801 <shimizu.kct@om.asahi-kasei.co.jp>
2016-02-26 18:08:08 +0900

    to be ask password magit push when windows

--- init.d/10_magit.el
+++ init.d/10_magit.el
@@ -1,6 +1,8 @@
 ;; magit
 (when (package-installed-p 'magit)
   (require 'magit)
+  (when (eq (window-system) 'w32)
+    (setenv "GIT_ASKPASS" "git-gui--askpass"))
   (global-magit-file-mode t)
   (add-hook 'eshell-mode-hook 'magit-file-mode)
   )
