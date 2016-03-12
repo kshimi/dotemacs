0c348b78f5f8aed28e31a860ea84b298508807df
SHIMIZU Katsuya <katsuyashimizu@gmail.com>
2016-03-12 21:55:40 +0900

    improve package archive url

--- init.el
+++ init.el
@@ -4,7 +4,9 @@
 ;;; Code:
 ;(setq debug-on-error t)
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
 (package-initialize)
 
 (custom-set-variables
