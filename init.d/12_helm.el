3c64f456c179242a1707ac73e3d2a65edf933e00
t0962801 <shimizu.kct@om.asahi-kasei.co.jp>
2016-10-28 09:32:06 +0900

    set helm-ag command to pt

--- init.d/12_helm.el
+++ init.d/12_helm.el
@@ -18,6 +18,8 @@
   (defadvice helm-buffers-sort-transformer (around ignore activate)
     (setq ad-return-value (ad-get-arg 0)))
 
+  (setq helm-ag-base-command "pt --nocolor --nogroup")
+
   (global-set-key (kbd "C-x C-f") 'helm-find-files)
   (global-set-key (kbd "M-x") 'helm-M-x)
   (global-set-key (kbd "M-y") 'helm-show-kill-ring)
