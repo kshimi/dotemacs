784962866e0a5c9a0d3e8fc9b04fd66a57f9323d
SHIMIZU Katsuya <katsuyashimizu@gmail.com>
2018-02-10 21:36:38 +0900

    add package easy-kill

---
+++ init.d/10_easykill.el
@@ -0,0 +1,8 @@
+;;; Commentary: easy-kill
+;;; Code:
+(use-package easy-kill
+  :ensure t
+  :config
+  (global-set-key [remap kill-ring-save] 'easy-kill)
+  (global-set-key [remap mark-sexp] 'easy-mark)
+  )
