1e58b2da326a53ecb17c83691428caf54b138d6a
SHIMIZU Katsuya <katsuyashimizu@gmail.com>
2018-02-21 23:29:05 +0900

    improve ace-isearch settings

--- init.d/10_ace-isearch.el
+++ init.d/10_ace-isearch.el
@@ -3,5 +3,8 @@
 (use-package ace-isearch
   :config
   (global-ace-isearch-mode +1)
+  (custom-set-variables
+   '(ace-isearch-function 'avy-goto-char)
+   '(ace-isearch-use-jump 'printing-char))
   :ensure t
   )
