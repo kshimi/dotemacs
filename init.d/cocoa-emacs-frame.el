afef7aed551065f50399739e6fdecec9a0401801
SHIMIZU Katsuya <katsuyashimizu@gmail.com>
2018-06-14 22:48:22 +0900

    improve im settings

--- init.d/cocoa-emacs-frame.el
+++ init.d/cocoa-emacs-frame.el
@@ -3,12 +3,6 @@
       '((top . 22) (left . 840)
 	(height . 99) (width . 240)))
 
-;; set input method for mac
-;;(setq default-input-method "MacOSX")
-(require 'mozc)
-(setq default-input-method "japanese-mozc")
-
-
 ;; set Command key for Meta
 (when (eq system-type 'darwin)
   (setq ns-command-modifier (quote meta)))
