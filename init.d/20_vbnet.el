b0e8a3fa7ce2dff69ed6b2612123257b4ad3c230
SHIMIZU Katsuya <katsuyashimizu@gmail.com>
2018-10-08 22:53:25 +0900

    adjust color for vbnet mode

--- init.d/20_vbnet.el
+++ init.d/20_vbnet.el
@@ -1,3 +1,7 @@
 (autoload 'vbnet-mode "vbnet-mode" "Mode for editing VB.NET code." t)
 (setq auto-mode-alist (append '(("\\.\\(frm\\|bas\\|cls\\|vb\\)$" .
 				 vbnet-mode)) auto-mode-alist))
+(custom-set-faces
+ '(vbnet-funcall-face ((t (:foreground "cyan"))))
+ '(vbnet-namespace-face ((t (:foreground "dark orange")))))
+
