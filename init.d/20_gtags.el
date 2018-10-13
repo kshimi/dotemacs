028a21bfdddf68a75c5b9a9716e5d85ead19cd20
SHIMIZU Katsuya <katsuyashimizu@gmail.com>
2018-10-13 23:28:49 +0900

    add package helm-gtags

---
+++ init.d/20_gtags.el
@@ -0,0 +1,14 @@
+(use-package helm-gtags
+  :config
+  (add-hook 'ruby-mode-hook 'helm-gtags-mode)
+  (with-eval-after-load 'helm-gtags
+    (define-key helm-gtags-mode-map (kbd "M-t") 'helm-gtags-find-tag)
+    (define-key helm-gtags-mode-map (kbd "M-r") 'helm-gtags-find-rtag)
+    (define-key helm-gtags-mode-map (kbd "M-s") 'helm-gtags-find-symbol)
+    (define-key helm-gtags-mode-map (kbd "M-g M-p") 'helm-gtags-parse-file)
+    (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+    (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
+    (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack))
+   '(helm-gtags-suggested-key-mapping t))
+  :ensure
+  )
