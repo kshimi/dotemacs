38dba40010a423263a28897822480e781cdcd30e
SHIMIZU Katsuya <shimizu.kct@om.asahi-kasei.co.jp>
2018-10-14 23:26:15 +0900

    fix gtags settings

--- init.d/20_gtags.el
+++ init.d/20_gtags.el
@@ -1,6 +1,7 @@
 (use-package helm-gtags
   :config
   (add-hook 'ruby-mode-hook 'helm-gtags-mode)
+  (add-hook 'c-mode-common-hook 'helm-gtags-mode)
   (with-eval-after-load 'helm-gtags
     (define-key helm-gtags-mode-map (kbd "M-t") 'helm-gtags-find-tag)
     (define-key helm-gtags-mode-map (kbd "M-r") 'helm-gtags-find-rtag)
@@ -9,6 +10,5 @@
     (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
     (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
     (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack))
-   '(helm-gtags-suggested-key-mapping t))
-  :ensure
+  :ensure t
   )
