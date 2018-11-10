e0611e824b2d62e3624b82450545754fa32cb597
kshimi <katsuyashimizu@gmail.com>
2018-11-10 15:29:17 +0900

    improve vbnet-mode settings

--- init.d/30_vbnet.el
+++ init.d/30_vbnet.el
@@ -1,6 +1,11 @@
 (autoload 'vbnet-mode "vbnet-mode" "Mode for editing VB.NET code." t)
 (setq auto-mode-alist (append '(("\\.\\(frm\\|bas\\|cls\\|vb\\)$" .
 				 vbnet-mode)) auto-mode-alist))
-(set-face-attribute 'vbnet-funcall-face nil :foreground "light sky blue")
-(set-face-attribute 'vbnet-namespace-face nil :foreground "dark orange")
+(defun my-vbnet-mode-fn ()
+  "My ook for VB.NET mode."
+  (interactive)
+  (set-face-attribute 'vbnet-funcall-face nil :foreground "light sky blue" :background "#161822")
+  (set-face-attribute 'vbnet-namespace-face nil :foreground "dark orange" :background "#161822")
+  )
+(add-hook 'vbnet-mode-hook 'my-vbnet-mode-fn)
 
