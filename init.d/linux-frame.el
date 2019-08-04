954f2717cdcd5202ee5a2a9cc68afa6f338f1ef7
SHIMIZU Katsuya <katsuyashimizu@gmail.com>
2019-08-04 22:06:33 +0900

    modify linux settings

--- init.d/linux-frame.el
+++ init.d/linux-frame.el
@@ -1,12 +1,12 @@
 ;; set frame size
 (setq initial-frame-alist
       '((top . 0) (left . 0)
-	(height . 58) (width . 82)
+	(height . 45) (width . 82)
         (alpha . (95 . 85))))
 
 (set-face-attribute 'default nil
 		    :family "Ricty"
-		    :height 100)
+		    :height 120)
 (set-fontset-font (frame-parameter nil 'font)
 		  'japanese-jisx0208
 		  (cons "Ricty" "iso10646-1"))
