4f1506c7c3a0ad68168052b059322d2f0b9fa58b
SHIMIZU Katsuya <katsuyashimizu@gmail.com>
2019-08-21 10:21:28 +0900

    remove old settings for migemo

--- init.d/10_migemo.el
+++ init.d/10_migemo.el
@@ -12,14 +12,10 @@
     (setq migemo-dictionary "/usr/share/cmigemo/utf-8/migemo-dict")
     )
   (when (memq window-system '(w32))
-    (if (equal system-name "PCS40513")
-	(progn
-	  (setq migemo-command "d:/software/cmigemo-default-win32/cmigemo.exe")
-	  (setq migemo-dictionary "D:/software/cmigemo-default-win32/dict/utf-8/migemo-dict"))
-      (progn
-	(setq migemo-command "c:/Apps/cmigemo-default-win64/cmigemo.exe")
-	(setq migemo-dictionary "c:/Apps/cmigemo-default-win64/dict/utf-8/migemo-dict")
-	)))
+    (progn
+      (setq migemo-command "c:/Apps/cmigemo-default-win64/cmigemo.exe")
+      (setq migemo-dictionary "c:/Apps/cmigemo-default-win64/dict/utf-8/migemo-dict")
+      ))
   (setq migemo-options '("-q" "--emacs"))
   (setq migemo-user-dictionary nil)
   (setq migemo-regex-dictionary nil)
