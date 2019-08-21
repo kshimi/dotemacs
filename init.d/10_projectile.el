733ae9aef07d70dc21f0c7c76e409a053712fa94
SHIMIZU Katsuya <katsuyashimizu@gmail.com>
2019-08-21 10:19:45 +0900

    enable counsel-projectile

--- init.d/10_projectile.el
+++ init.d/10_projectile.el
@@ -6,6 +6,7 @@
   (setq projectile-completion-system 'ivy)
   :config
   (projectile-mode +1)
+  (counsel-projectile-mode +1)
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
   (use-package counsel-projectile :ensure t)
   (use-package projectile-rails
