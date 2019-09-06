3f69b18787677d47d23603543b513ee4417fa5d0
shimizu.katsuya <shimizu.katsuya@sdtech.co.jp>
2019-09-06 15:51:10 +0900

    apply jump storategy to lsp-ui-peek for python mode

--- init.d/40_smart-jump.el
+++ init.d/40_smart-jump.el
@@ -3,4 +3,11 @@
 (use-package smart-jump
   :ensure t
   :config
-  (smart-jump-setup-default-registers))
+  (smart-jump-register
+   :modes 'python-mode
+   :jump-fn 'lsp-ui-peek-find-definitions
+   :refs-fn 'lsp-ui-peek-find-references
+   :heuristic 'point
+   :order 1
+   )
+  )
