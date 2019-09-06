4bc79e07965e830c11242a6895b868e85f52698f
shimizu.katsuya <shimizu.katsuya@sdtech.co.jp>
2019-09-06 15:58:36 +0900

    improve lsp-mode settings

--- init.d/40_flycheck.el
+++ init.d/40_flycheck.el
@@ -2,10 +2,7 @@
   :ensure t
   :if (not (memq window-system '(w32)))
   :init
-  (use-package flycheck-pos-tip :ensure t)
   :config
   (global-flycheck-mode)
-  (with-eval-after-load 'flycheck
-    (flycheck-pos-tip-mode))
   )
 
--- init.d/45_lsp.el
+++ init.d/45_lsp.el
@@ -5,9 +5,17 @@
   :hook
   (python-mode . lsp)
   (ruby-mode . lsp)
+  :init
+  (setq lsp-enable-indentation nil)
+  (setq lsp-enable-snippet nil)
+  (setq lsp-enable-on-type-formatting nil)
   :custom
-  (lsp-prefer-flymake 'flymake)
+  (lsp-prefer-flymake :none)
   :commands lsp)
-
 (use-package lsp-ui :ensure t :commands lsp-ui-mode)
-(use-package company-lsp :ensure t :commands company-lsp)
+(use-package company-lsp
+  :ensure t
+  :commands company-lsp
+  :custom
+  (company-lsp-cache-candidates nil)
+  (company-lsp-enable-recompletion nil))
