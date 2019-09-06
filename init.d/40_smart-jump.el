a734bac11b64ce740f28077d050c1d752319e49e
SHIMIZU Katsuya <katsuyashimizu@gmail.com>
2019-09-06 19:18:25 +0900

    amend smart-jump settings

--- init.d/40_smart-jump.el
+++ init.d/40_smart-jump.el
@@ -4,10 +4,9 @@
   :ensure t
   :config
   (smart-jump-register
-   :modes 'python-mode
+   :modes 'lsp-ui-mode
    :jump-fn 'lsp-ui-peek-find-definitions
+   :pop-fn nil
    :refs-fn 'lsp-ui-peek-find-references
-   :heuristic 'point
-   :order 1
-   )
-  )
+   :heuristic 'error))
+
