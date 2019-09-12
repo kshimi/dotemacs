8046fa4b1f347539803bbe30457816dd25952efd
SHIMIZU Katsuya <katsuyashimizu@gmail.com>
2019-09-12 23:05:48 +0900

    inhibit flymake when not in windows

--- init.d/40_flymake.el
+++ init.d/40_flymake.el
@@ -1,6 +1,7 @@
 ;;; Commentary: flymake settings
 ;;; Code:
 (use-package flymake-diagnostic-at-point
+  :if (memq window-system '(w32))
   :ensure t
   :hook
   (flymake-mode . flymake-diagnostic-at-point-mode))
