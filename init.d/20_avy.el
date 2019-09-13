b43ac9977ab3b5ef38d98954346410f9199f25fa
SHIMIZU Katsuya <katsuyashimizu@gmail.com>
2019-09-13 15:40:17 +0900

    improve use-package description

--- init.d/20_avy.el
+++ init.d/20_avy.el
@@ -1,16 +1,12 @@
 ;;; Commentary: avy
 ;;; Code:
-(use-package avy
-  :bind (
-	 ("M-g c" . avy-goto-char)
-	 ("M-g f" . avy-goto-line)
-	 ("M-g w" . avy-goto-word-1)
-	 ("M-g e" . avy-goto-word-0))
-  :config
-  (avy-setup-default)
-  (use-package avy-zap
-    :bind (("M-z" . avy-zap-to-char))
-    :ensure t
-    )
-  :ensure t
-  )
+(use-package avy :ensure t
+  :bind
+  ("M-g c" . avy-goto-char)
+  ("M-g f" . avy-goto-line)
+  ("M-g w" . avy-goto-word-1)
+  ("M-g e" . avy-goto-word-0)
+  :config (avy-setup-default))
+
+(use-package avy-zap :ensure t
+  :bind ("M-z" . avy-zap-to-char))
