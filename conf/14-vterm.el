242c3eec267229626995ed4d409790b06b4ace00
Shimizu Katsuya <katsuyashimizu@gmail.com>
2026-03-05 10:25:18 +0900

    add vterm mode

---
+++ conf/14-vterm.el
@@ -0,0 +1,14 @@
+(use-package vterm
+  :commands vterm
+  :custom
+  (vterm-max-scrollback 10000))
+
+;; Claude Codeなど、特定のツールを快適に使うための
+;; ターミナル内でのキーバインド競合回避
+(add-hook 'vterm-mode-hook
+          (lambda ()
+	    (display-line-numbers-mode -1)
+	    (visual-line-mode -1)
+            (setq-local confirm-kill-processes nil)
+            ;; Emacsのキーバインドを優先させたい場合はここで設定
+            (define-key vterm-mode-map (kbd "C-y") 'vterm-yank)))
